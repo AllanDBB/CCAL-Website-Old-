d436380dc95056fb46219e7d98054280cf7d07b1
Allan! <adbyb.es@gmail.com>
2024-01-24 16:33:33 -0600

    Access pages frontend 70%

--- ccalajuela_web_frontend/src/App.js
+++ ccalajuela_web_frontend/src/App.js
@@ -5,7 +5,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/navbar/Navbar';
 import AboutUs from './pages/About-us';
-import Access from './pages/Access';
+import SignIn from './pages/Access/Sign-in';
+import SignUp from './pages/Access/SignUp';
+import TermsAndConditions from './components/conditions/TermsAndConditions';
+import ForgotPassword from './pages/Access/ForgotPassword';
+import RestorePassword from './pages/Access/RestorePassword';
 
 import './App.css';
 
@@ -13,11 +17,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/conocenos" element={<AboutUs />} />
-          <Route path="/acceder" element={<Access />} />
+      <Routes>
+          <Route path="/" element={<><Navbar /><Home /></>} />
+          <Route path="/conócenos" element={<><Navbar /><AboutUs /></>} />
+          <Route path="/iniciar-sesión" element={<SignIn />} />
+          <Route path="/registrarse" element={<SignUp />} />
+          <Route path="/términos-condiciones" element={<TermsAndConditions />} />
+          <Route path="/olvidé-mi-contraseña" element={<ForgotPassword />} />
+          <Route path="/restaurar-contraseña" element={<RestorePassword />} />
         </Routes>
       </div>
     </Router>
---
+++ ccalajuela_web_frontend/src/pages/Access/ForgotPassword.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './ForgotPassword.css';
+import Logo from '../assets/Logo.svg';
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para manejar la solicitud de recuperación de contraseña
+  };
+
+  return (
+    <div className="container-olvido-contrasena">
+      <form className="formulario-olvido-contrasena" onSubmit={handleSubmit}>
+        <div className="logo-ccal">
+          <img src={Logo} alt="Logo CCAL"/>
+        </div>
+
+        <div className="columna-flexible">
+          <label className="etiqueta">Correo electrónico</label>
+          <div className="contenedor-input">
+            <input
+              type="email"
+              className="campo-input"
+              placeholder="Ingresa tu correo electrónico"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <button className="boton-enviar">Enviar confirmación de recuperación</button>
+
+        <p className="p">
+          ¿Recordaste tu contraseña?   
+          <NavLink to="/iniciar-sesión" className="texto-enlace"> 
+             Iniciar sesión
+          </NavLink>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPassword;
