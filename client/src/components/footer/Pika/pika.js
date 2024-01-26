6b16a30d5c1c9a12aed4378393307319c9ceb7d2
JpUser1234 <jeanpaulseqsal@gmail.com>
2024-01-26 03:01:31 -0600

    Bnas noshes :D
    Estoy cansado jefe...

--- client/src/components/footer/Pika/pika.js
+++ client/src/components/footer/Pika/pika.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './pika.css'; // Asegúrate de tener la ruta correcta al archivo CSS
 
 const Loader = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className="loader-pika">
+    <div
+      className={`loader-pika ${isHovered ? 'show' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="loader-before"></div>
       <div className="loader-after"></div>
     </div>
