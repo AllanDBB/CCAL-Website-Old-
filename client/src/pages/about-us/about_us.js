089f5001f874f7dae52aa80441964c877ac711c1
JpUser1234 <jeanpaulseqsal@gmail.com>
2024-01-26 12:49:17 -0600

    xd

--- client/src/pages/about-us/about_us.js
+++ client/src/pages/about-us/about_us.js
@@ -37,16 +37,20 @@ const AboutUs = () => {
         
         <div className="historia">
           <div className="historiaPicture"><img src={HistoryPicture} alt="Descripción de la imagen" style={{ width: '45%', height: 'auto' }}/></div>
-          <div className="historiaTitulo">
-              <div className="title1">Nuestra Historia</div>
-              <History />
-          </div>
 
-          <div className="historiaBody">
-            <text>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </text>
+          <div className='historiaText'>
+            <div className="historiaTitulo">
+                <div className="title1">Nuestra Historia</div>
+                <History />
+            </div>
+
+            <div className="historiaBody">
+              <text>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </text>
+            </div>
           </div>
+  
 
           
         </div>
