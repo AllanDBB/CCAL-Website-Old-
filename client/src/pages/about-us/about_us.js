96c676da9d57785ab6f35290f102b06c03defeb0
JpUser1234 <jeanpaulseqsal@gmail.com>
2024-01-26 19:02:58 -0600

    arreglo globo raro ese si xd

--- client/src/pages/about-us/about_us.js
+++ client/src/pages/about-us/about_us.js
@@ -44,8 +44,7 @@ const AboutUs = () => {
             </div>
             <div className="equipoStructure">
               <div className="equipoTitle">
-              <div className="title2">Nuestro Equipo</div>
-                
+                <div className="title2">Nuestro Equipo</div>
               </div>
 
               <div className="equipoBody">
@@ -68,11 +67,13 @@ const AboutUs = () => {
                   
               </div>
 
+              
               <div className="historiaBody">
+              <element><History/></element>
                 <text>
                   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </text>
-                <element><History/></element>
+                
               </div>
             
             </div>
