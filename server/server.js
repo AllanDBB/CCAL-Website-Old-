31e0868bb4ce0203aeeee93aa5ff113ad84ab4b9
Allan! <adbyb.es@gmail.com>
2024-01-29 14:24:48 -0600

    update backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectDB = require('./src/database'); 
-const uploadHome = require('./src/uploadHome');
-const uploadAchievements = require('./src/uploadAchievements.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const multer = require('multer');
-
+const connectDB = require('./src/database');
+const uploadHome = require('./src/uploadHome');
+const uploadAchievements = require('./src/uploadAchievements');
+const User = require('./src/models/user'); 
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -16,29 +18,62 @@ app.use(bodyParser.json());
 
 // Testing route.
 app.get('/', (req, res) => {
-  res.send('Prueba pasada.');
+    res.send('Prueba pasada.');
 });
 
 // Route to upload to home banner files.
-
 app.post('/uploadHome', uploadHome.single('file'), (req, res) => {
-
-  console.log('uploadPath:', req.body.uploadPath);
-  res.send('Archivo subido con éxito!')
-
+    console.log('uploadPath:', req.body.uploadPath);
+    res.send('Archivo subido con éxito!');
 });
 
 // Route to upload to achievements banner files.
-
 app.post('/uploadAchievements', uploadAchievements.single('file'), (req, res) => {
-
-  console.log('uploadPath:', req.body.uploadPath);
-  res.send('Archivo subido con éxito!')
-
+    console.log('uploadPath:', req.body.uploadPath);
+    res.send('Archivo subido con éxito!');
 });
 
+// Ruta de registro
+app.post('/register', async (req, res) => {
+    try {
+        const { nombre, apellidos, email, password, confirmPassword } = req.body;
+        if (password !== confirmPassword) {
+            return res.status(400).send('Las contraseñas no coinciden');
+        }
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send('El email ya está registrado');
+        }
+        const newUser = new User({ nombre, apellidos, email, password });
+        await newUser.save();
+
+        res.status(201).send('Usuario registrado con éxito');
+    } catch (error) {
+        res.status(500).send('Error al registrar usuario: ' + error.message);
+    }
+});
+
+// Ruta de inicio de sesión
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send('Email o contraseña incorrectos');
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).send('Email o contraseña incorrectos');
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).send('Error al iniciar sesión: ' + error.message);
+    }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${port}`);
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
---
+++ server/src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    lastnames: {
+
+        type: String,
+        required: true
+    },
+    email:{
+
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+
+        type: String,
+        required: true
+    },
+    roles: [{
+        type: String,
+        enum: ['administrativo', 'profesor', 'desarrollador', 'egresado', 'estudiante activo', 'usuario'],
+        default: 'user'
+    }]
+});
+
+// Pre-save hook para hashear la contraseña antes de guardar el usuario.
+UserSchema.pre('save', async function (next){
+    if (this.isModified('password') || this.isNew ){
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+    }
+    next();
+});
+
+// Método para verificar la contraseña.
+
+UserSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
